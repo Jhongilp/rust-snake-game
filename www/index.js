f19578e5ae5433d95afb960bdf8ad5fcc3471a8e
Jonathan Gil <jhongilp@gmail.com>
2023-03-30 17:46:26 -0500

    add canvas

--- www/index.js
+++ www/index.js
@@ -1,5 +1,31 @@
-import init, { greet } from "snake_game";
+import init, { World } from "snake_game";
 
 init().then(() => {
-  greet("Jonathan");
+  const world = World.new();
+  const worldWidth = world.width();
+
+  const CELL_SIZE = 20;
+
+  const canvas = document.getElementById("snake-game-canvas");
+  canvas.height = worldWidth * CELL_SIZE;
+  canvas.width = worldWidth * CELL_SIZE;
+  const ctx = canvas.getContext("2d");
+
+  function drawWorld() {
+    ctx.beginPath();
+
+    for (let x = 0; x < worldWidth + 1; x++) {
+      ctx.moveTo(CELL_SIZE * x, 0);
+      ctx.lineTo(CELL_SIZE * x, worldWidth * CELL_SIZE);
+    }
+
+    for (let y = 0; y < worldWidth + 1; y++) {
+      ctx.moveTo(0, CELL_SIZE * y);
+      ctx.lineTo(worldWidth * CELL_SIZE, CELL_SIZE * y);
+    }
+
+    ctx.stroke();
+  }
+
+  drawWorld();
 });
