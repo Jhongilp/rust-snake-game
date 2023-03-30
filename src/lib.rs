3d78661886dbea9831f786f69a8064362819c111
Jonathan Gil <jhongilp@gmail.com>
2023-03-30 17:46:15 -0500

    add World struct

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,18 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(name);
+pub struct World {
+    width: usize,
 }
 
 #[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
+impl World {
+    pub fn new() -> World {
+        World { width: 16 }
+    }
+
+    pub fn width(&self) -> usize {
+        self.width
+    }
 }
