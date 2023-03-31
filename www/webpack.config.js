d40cd90ec738110559eda01f1a4dad919ce1fd9f
Jonathan Gil <jhongilp@gmail.com>
2023-03-31 16:59:54 -0500

    update config files to work with TS

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -2,12 +2,24 @@ const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: "./index.js",
+  entry: "./index.ts",
   output: {
     path: path.resolve(__dirname, "public"),
     filename: "index.js"
   },
   mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
   plugins: [
     new CopyWebpackPlugin({
       patterns: [
